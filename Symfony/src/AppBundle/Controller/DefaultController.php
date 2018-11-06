ae6dd632c330ab3a6731ae1e89b8d43275d8763e
DESKTOP-A5CERV0\Joan <jobanyuls@gmail.com>
2018-11-06 19:58:04 +0100

    Haciendo

--- Symfony/src/AppBundle/Controller/DefaultController.php
+++ Symfony/src/AppBundle/Controller/DefaultController.php
@@ -5,7 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -15,8 +15,8 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('frontal/index.html.twig');//, [
+            //'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        //]);
     }
 }
