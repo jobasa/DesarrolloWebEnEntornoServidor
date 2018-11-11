a45e6e3d35f2c46cf4fd52c6d5380be3fa892a43
DESKTOP-A5CERV0\Joan <jobanyuls@gmail.com>
2018-11-11 18:14:13 +0100

    Haciendo

--- Symfony/src/AppBundle/Controller/DefaultController.php
+++ Symfony/src/AppBundle/Controller/DefaultController.php
@@ -19,4 +19,47 @@ class DefaultController extends Controller
             //'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         //]);
     }
+
+    /**
+     * @Route("/", name="registro")
+     */
+    public function registroAction(Request $request){
+      return $this->render('frontal/registro.html.twig');
+    }
+
+    /**
+     * @Route("/", name="login")
+     */
+     public function loginAction(Request $request){
+       return $this->render('frontal/login.html.twig');
+     }
+
+     /**
+      * @Route("/", name="crearEventos")
+      */
+      public function crearEventosAction(Request $request){
+        return $this->render('frontal/crearEventos.html.twig');
+      }
+
+      /**
+       * @Route("/", name="eventos")
+       */
+       public function eventosAction(Request $request){
+         return $this->render('frontal/eventos.html.twig');
+       }
+
+       /**
+        * @Route("/", name="admin")
+        */
+        public function adminAction(Request $request){
+          return $this->render('frontal/admin.html.twig');
+        }
+
+        /**
+         * @Route("/", name="cuenta")
+         */
+         public function cuentaAction(Request $request){
+           return $this->render('frontal/cuenta.html.twig');
+         }
+
 }
