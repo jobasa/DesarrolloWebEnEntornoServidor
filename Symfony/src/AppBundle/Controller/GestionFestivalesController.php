7cce1a26146d39260f94173f67a2d6b8bfce664b
DESKTOP-A5CERV0\Joan <jobanyuls@gmail.com>
2018-12-10 20:16:56 +0100

    insertar terminado

--- Symfony/src/AppBundle/Controller/GestionFestivalesController.php
+++ Symfony/src/AppBundle/Controller/GestionFestivalesController.php
@@ -25,6 +25,22 @@ class GestionFestivalesController extends Controller
       //construyendo el formulario
       $form = $this->createForm(FestivalType::class, $festival);
 
+      //Recogemos la informacion
+      $form->handleRequest($request);
+      if ($form->isSubmitted() && $form->isValid()) {
+        //Rellenar el Entity Festival
+        $festival = $form->getData();
+        $festival->setDatos("");
+        $festival->setFoto("");
+        $festival->setTop(0);
+
+        //Almacenar nuevo festival
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($festival);
+        $em->flush();
+        return $this->redirectToRoute('festival',array('id' => $festival->getId()));
+      }
+
       // replace this example code with whatever you need
       return $this->render('gestionFestivales/nuevoFestival.html.twig',array('form' => $form->createView()));
   }
